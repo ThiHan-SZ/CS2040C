45262f0b136a726098c57c4827d16efc3557bdc6
The_Steel_Sentinel <rubythihan@gmail.com>
2026-03-16 00:17:26 +0800

    PS4 : Full Answer Simanumer

---
+++ P4Projects/simanumer.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    size_t n = 0;
+    cin >> n;
+    vector<string> phoneRecords(n);
+    for (size_t i = 0; i < n; i++) {
+        cin >> phoneRecords[i];
+    }
+    size_t q = 0;
+    cin >> q;
+    vector<string> queries(q);
+    for (size_t i = 0; i < q; i++) {
+        cin >> queries[i];
+    }
+
+    unordered_map<string, long> prefixMap;
+    for (size_t i = 0; i < n; i++) {
+        for(size_t j = 0; j < phoneRecords[i].length(); j++) {
+            string prefix = phoneRecords[i].substr(0, j + 1);
+            prefixMap[prefix] += 1;
+        }
+    }
+    for (size_t i = 0; i < q; i++) {
+        cout << prefixMap[queries[i]] << endl;
+    }
+    return 0;
+}
